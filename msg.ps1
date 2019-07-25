ae581a635b010528b33dae75ea8afbec4420240f
Paul Jakob <37003325+TheJaakob@users.noreply.github.com>
2019-07-25 10:51:34 +0200

    Update and rename msg.txt to msg.ps1

--- msg.ps1
+++ msg.ps1
@@ -1,9 +1,8 @@
-PCLIST = Get-ADComputer -Filter *| Select -ExpandProperty DNSHostname
+$PCLIST = Get-ADComputer -Filter *| Select -ExpandProperty DNSHostname
 
 ForEach ($computer in $PCLIST) {
 
         $CmdMessage = {C:\windows\system32\msg.exe * /server:$computer 'Dies ist ein Test'}
 
         $CmdMessage | Invoke-Expression
-    }
-
+}
